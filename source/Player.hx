cde560bcf5ad627236266d97b4b8d8ddff874010
NAEL2XD <108278470+NAEL2XD@users.noreply.github.com>
2025-06-29 22:13:36 +0200

    Build/Commit 2

--- source/Player.hx
+++ source/Player.hx
@@ -3,20 +3,19 @@ package;
 class Player extends FlxSprite {
     var velocityMovement:Array<Float> = [0, 0];
     final key:Array<FlxKey> = [LEFT, DOWN, UP, RIGHT, A, S, W, D];
-
+    
     public function new() {
         super();
         makeGraphic(32, 32, 0xFF00FF00);
     }
-
+    
     public function checkMovement() {
-
-        velocity.x = 0;
-        velocity.y = 0;
-
         var i:Int = 0;
+
         for (k in key) {
-            if (FlxG.keys.anyJustPressed([k])) {
+            if (FlxG.keys.anyPressed([k])) {
+                trace("Pressed!");
+
                 switch(i % 4) {
                     case 0: velocityMovement[0] -= 0.5;
                     case 1: velocityMovement[1] -= 0.5;
@@ -35,5 +34,12 @@ class Player extends FlxSprite {
 
             i++;
         }
+
+        velocityMovement[0] /= 1.1;
+        velocityMovement[1] /= 1.1;
+        x += velocityMovement[0];
+        y += velocityMovement[1];
+
+        trace(velocityMovement);
     }
 }
