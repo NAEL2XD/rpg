2a19a0e7002e0795db16615da9c41ad413209c29
NAEL2XD <108278470+NAEL2XD@users.noreply.github.com>
2025-07-03 00:02:54 +0200

    Build/Commit 24

--- source/Main.hx
+++ source/Main.hx
@@ -8,8 +8,7 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(640, 320, Chapter1_1, 60, 60, true));
-
 		CrashHandler.init();
+		addChild(new FlxGame(640, 320, Chapter1_1, 60, 60, true));
 	}
 }
