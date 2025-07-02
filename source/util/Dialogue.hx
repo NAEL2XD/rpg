9cdddf39b392f5c4f3c041a8e0b2b3797a18085f
NAEL2XD <108278470+NAEL2XD@users.noreply.github.com>
2025-07-03 00:59:23 +0200

    Build/Commit 45

--- source/util/Dialogue.hx
+++ source/util/Dialogue.hx
@@ -1,15 +1,23 @@
 package util;
 
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
 import flixel.FlxObject;
 import sys.io.File;
 import haxe.Json;
 import flixel.FlxSubState;
 
 class Dialogue extends FlxSubState {
-    var dialogueText:String = "";
     var spr:FlxObject = null;
+    var timer:FlxTimer = new FlxTimer();
+    var textSpr:FlxText = null;
+
+    var dialogueText:String = "";
+    var done:Bool = false;
 
     public function new(dialogueID:String, sprite:FlxObject) {
+        done = false;
+
         var list = Json.parse(File.getContent("assets/data/dialogue.json"));
         var dial:Array<Dynamic> = list.dialogueList;
 
@@ -31,29 +39,59 @@ class Dialogue extends FlxSubState {
 
     override function create() {
         var s:FlxSprite = null;
-        var l:Int = 256;
+        final l:Int = 256;
+        final pos:Float = spr.x - 40;
 
         s = new FlxSprite().makeGraphic(l, 82, 0xFFFFFFFF);
-        s.x = spr.x - 60;
+        s.x = pos - 60;
         s.y = spr.y - 90;
         add(s);
 
         s = new FlxSprite().makeGraphic(l + 20, 60, 0xFFFFFFFF);
-        s.x = spr.x - 70;
+        s.x = pos - 70;
         s.y = spr.y - 80;
         add(s);
 
         s = new FlxSprite().makeGraphic(l - 8, 72, 0xFF000000);
-        s.x = spr.x - 56;
+        s.x = pos - 56;
         s.y = spr.y - 85;
         add(s);
 
+        final x = s.x + 8;
+        final y = s.y + 8;
+
         s = new FlxSprite().makeGraphic(l + 12, 52, 0xFF000000);
-        s.x = spr.x - 66;
+        s.x = pos - 66;
         s.y = spr.y - 76;
         add(s);
+
+        textSpr = new FlxText(x, y, l - 8, "").setFormat("assets/fonts/main.ttf", 12);
+        add(textSpr);
+
+        var ind:Int = -1;
+        timer.start(0.12, e -> {
+            ind++;
+            textSpr.text += dialogueText.charAt(ind);
+
+            if (e.loopsLeft == 0) {
+                done = true;
+            }
+        }, dialogueText.length);
         
         trace(dialogueText);
         super.create();
+    }
+
+    override function update(elapsed:Float) {
+        if (FlxG.keys.justPressed.ANY) {
+            if (done) {
+                close();
+            } else {
+                timer.cancel();
+                textSpr.text = dialogueText;
+            }
+        }
+
+        super.update(elapsed);
     }
 }
